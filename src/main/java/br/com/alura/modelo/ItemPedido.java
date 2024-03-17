9cc1b0d641db33c2b236a56a9ccd73557fd55398
Gabriel <grodrigues256@gmail.com>
2024-03-17 11:22:16 -0300

    feat(m01-2024-03-17) relacionamento many-to-many

---
+++ src/main/java/br/com/alura/modelo/ItemPedido.java
@@ -0,0 +1,34 @@
+package br.com.alura.modelo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "itens_pedidos")
+@Data
+@FieldNameConstants
+@NoArgsConstructor
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal precoUnitario;
+    private Integer quantidade;
+
+    @ManyToOne
+    private Produto produto;
+
+    @ManyToOne
+    private Pedido pedido;
+
+    public ItemPedido(Integer quantidade, Produto produto, Pedido pedido) {
+        this.quantidade = quantidade;
+        this.produto = produto;
+        this.pedido = pedido;
+    }
+}
