c6aa59bf8f13bc1e8b9b5d98147ef0d2e73b7d53
Gabriel <grodrigues256@gmail.com>
2024-03-30 14:40:49 -0300

    feat(m02-2024-03-30) consultas com funções de agregação

--- src/main/java/br/com/alura/dao/PedidoDao.java
+++ src/main/java/br/com/alura/dao/PedidoDao.java
@@ -3,6 +3,8 @@ package br.com.alura.dao;
 import br.com.alura.modelo.Pedido;
 import jakarta.persistence.EntityManager;
 
+import java.math.BigDecimal;
+
 public class PedidoDao {
 
     private final EntityManager entityManager;
@@ -17,6 +19,14 @@ public class PedidoDao {
 
     public Pedido buscarPorId(Long id) {
         return entityManager.find(Pedido.class, id);
+    }
+
+    public BigDecimal valorTotalVendido() {
+        String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+
+        return entityManager.createQuery(jpql, BigDecimal.class)
+                .getSingleResult();
+
     }
 
 }
--- src/main/java/br/com/alura/modelo/Pedido.java
+++ src/main/java/br/com/alura/modelo/Pedido.java
@@ -21,7 +21,7 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "valor_total")
-    private BigDecimal valorTotal;
+    private BigDecimal valorTotal = BigDecimal.ZERO;
     private LocalDate date = LocalDate.now();
 
     @ManyToOne
@@ -37,5 +37,6 @@ public class Pedido {
     public void adicionarItem(ItemPedido item) {
         item.setPedido(this);
         this.itens.add(item);
+        this.valorTotal = this.valorTotal.add(item.getValor());
     }
 }
